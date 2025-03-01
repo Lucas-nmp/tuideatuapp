2123f4ccf2e2da59098793ea52a50c0891b203c4
lucas-nmp <lucas.morandeira.parejo@gmail.com>
2025-03-01 20:14:07 +0100

    Add some parts

--- assets/app.js
+++ assets/app.js
@@ -5,6 +5,24 @@ import './bootstrap.js';
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
-import './styles/app.css';
+import './styles/johndoe.css';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+
+$(document).ready(function(){
+    $(".navbar .nav-link").on('click', function(event) {
+
+        if (this.hash !== "") {
+
+            event.preventDefault();
+
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 700, function(){
+                window.location.hash = hash;
+            });
+        } 
+    });
+});
